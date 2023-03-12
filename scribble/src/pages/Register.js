51143d2f4c64e08f5e117a48f9bafeef3a727486
VIZ-ART <85454870+VIZ-ART@users.noreply.github.com>
2023-03-12 17:26:49 +0530

    Registration logic and notifications

--- scribble/src/pages/Register.js
+++ scribble/src/pages/Register.js
@@ -1,7 +1,104 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Logo, FormRow } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { toast } from "react-toastify";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  user: "Student",
+  isMember: true,
+};
 
 function Register() {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(`${name}:${value}`);
+    setValues({ ...values, [name]: value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, password, user, isMember } = values;
+    if (!email || !password || (!isMember && !name))
+      toast.error("Please fill out all fields!");
+  };
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {values.isMember || (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        {values.isMember || (
+          <div>
+            <label htmlFor="User" className="form-label">
+              User
+            </label>
+            <select
+              type="select"
+              name="user"
+              value={values.user}
+              onChange={handleChange}
+              className="form-row"
+            >
+              <option value="Student">Student</option>
+              <option value="Teacher">Teacher</option>
+            </select>
+          </div>
+        )}
+
+        {/* {values.isMember || (
+          <FormSelectRow
+            type="select"
+            name="user"
+            value={values.user}
+            onChange={handleChange}
+            options={("Student", "Teacher")}
+            className="form-row"
+          />
+        )} */}
+
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
 }
 
 export default Register;
